d2cf70fa36a5598098a2a78bffbccec705029644
manish <manishkumarji85635@gmail.com>
2025-11-29 10:03:25 +0530

    create a insta like button

---
+++ day-46/script.js
@@ -0,0 +1,12 @@
+var img = document.querySelector("img");
+var heart = document.querySelector("#heart");
+
+img.addEventListener("dblclick", function(){
+    
+    heart.style.opacity = '1'
+    
+
+    setTimeout(function(){
+        heart.style.transform = 'translate(-50%,-300%) scale(1)'
+    }, 1000)
+})
