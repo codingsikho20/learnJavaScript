13c4011bffc9994bc0d3e938c1600b8346230159
manish <manishkumarji85635@gmail.com>
2025-12-20 10:22:56 +0530

    Advance js learn

---
+++ day-56/script.js
@@ -0,0 +1,145 @@
+// that is the first topic of this lesson 
+// that is the object kiya hota hai ab hum ise ache se padre hai gues cholo 
+// to ek student naam ka object bante hau 
+
+// const student = {
+//     name: 'Manish',
+//     roll: 101,
+//     study: function (){
+//         console.log("Manish is studying")
+//     },
+// };
+
+// student.study();
+
+// const car = {
+//     brand: "BMW",
+//     color: "black",
+//     start: function(){
+//         console.log("the car is starting right now.")
+//     }
+// }
+
+// car.start();ṇ
+
+// const smartWatch = {
+//     barandName: "noise",
+//     battary: "Lithium ayan",
+//     color: "blue",
+//     make: function(){
+//         console.log("make a watch every where");
+//     }
+// }
+
+// smartWatch.make();
+
+// that is the topic number second "class"
+
+// class Remote{
+//     constructor(){
+//         this.product = "daikin"
+//         this.price = 122
+//         this.color = "white"
+//     }
+
+//     powerOn(){
+//         console.log("the machine is on right now.");
+//     }
+
+//     powerOff(){
+//         console.log("the machine is off right now");
+        
+//     }
+
+// }
+
+// let remote = new Remote();
+
+
+// class Remote {
+//     constructor(product, price, color){
+//         this.product = product;
+//         this.price = price;
+//         this. color = color;
+//     }
+
+//     powerOn(){
+//         console.log("the machine is on right now");
+//     }
+
+//     powerOff(){
+//         console.log("machine is off rifht now.");
+//     }
+
+// }
+
+// let remote1 = new Remote("Lenove", 1220, "gray");
+
+// let remote2 = new Remote("daikin", 3421, "white");
+
+// class biscuitMaker{
+//     constructor(){
+//         this.name = "parle G"
+//         this.price = 5;
+//     }
+
+// }
+
+// let biscuit = new biscuitMaker();
+
+// class bante hai taaki hume har ek naye object milega 
+// classses object ki factory hai 
+// har bar 'NEW' word ke saath class run karega to ek naya object milega 
+
+// let's make aaloochat 
+// class Aloochat{
+//     constructor(){
+//         this.price = 55;
+//         this.oil = "5ml"
+//         this.oilBrand = "saffola"
+//         this.massala = ["pudina", "dhaniya", "maricha"]
+//     }
+
+//     pack(){
+//         console.log("Aloochat is paked");
+//     }
+
+//     unpacked(){
+//         console.log("aloochat unpacked");
+        
+//     }
+// } 
+
+
+// let ac1 = new Aloochat();
+
+// let make blueprint of books
+
+class Kitaab{
+    constructor(name, author, price, color){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.color = color;
+    }
+
+    panaPalato(){
+        console.log("logo ko hindi me science ki kitaab padhana acha nahi lagta hai");
+        
+    }
+
+    bookmarkLagao(){
+        console.log("hum hindi medium me padne wale logo ko n bookmark lagane me koi interest nahi hota hai");
+        
+    }
+
+    padhlo(){
+        console.log("20 pana padh liya ab rest karna hai");
+        
+    }
+
+}
+
+let k1 = new Kitaab("hindi", "Mr.Manish", "300NRI", "blue");
+let k2 = new Kitaab("math", "Mr.Kishan", "403NRI", "green");
+let k3 = new Kitaab("history", "Mr.Yashawant", "90NRI", "red");
